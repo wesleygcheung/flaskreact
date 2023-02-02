60ad146d6566ad1b650b47214395ff6c9ad54c89
Wesley C <wesley.g.cheung@gmail.com>
2023-02-02 00:28:55 -0800

    toggle authdomain for dev vs prod

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,9 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
+const auth_domain = window.location.hostname === 'localhost' ? "flaskreact-2c53b.firebaseapp.com" : "tastingroom.herokuapp.com"
+
 const firebaseConfig = {
     apiKey: "AIzaSyCm4Kn2kNenlE4TeNlmD9a79BVall5ujs0",
-    authDomain: "tastingroom.herokuapp.com",
+    authDomain: auth_domain,
     projectId: "flaskreact-2c53b",
     storageBucket: "flaskreact-2c53b.appspot.com",
     messagingSenderId: "113661173572",
